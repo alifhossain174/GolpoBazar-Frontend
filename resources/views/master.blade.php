dc90e5f28e9988a9e17bc64b69815d3e15ad1a50
Fahim <alifhossain174@gmail.com>
2025-04-06 18:29:47 +0200

    Updating Code

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="{{ url('assets') }}/css/toastr.min.css">
     <link rel="stylesheet" href="{{ url('assets') }}/css/swiper-bundle.min.css">
-    <link rel="stylesheet" href="{{ url('assets') }}/css/style.css">
+    <link rel="stylesheet" href="{{ url('assets') }}/css/style_v2.css">
     <link rel="stylesheet" href="{{ url('assets') }}/css/responsive.css">
 
     <style>
